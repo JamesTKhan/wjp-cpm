8795949c2e191d01024ac5772ce98c4ca5f5ba81
Laptop-NIFU207\James <wjamesfl@gmail.com>
2020-12-06 17:39:49 -0500

    Add application.properties, externalize configs with PropertyService. Had to convert switch statement to If/Else due to loaded props not being a compile time constant.

--- src/main/java/com/jpcodes/Main.java
+++ src/main/java/com/jpcodes/Main.java
@@ -1,10 +1,40 @@
 package com.jpcodes;
 
+import com.jpcodes.service.PropertyService;
+import com.jpcodes.service.PropertyServiceImpl;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
+    /**
+     * PropertyService for loading and retrieving props
+     **/
+    public static PropertyService propertyService = new PropertyServiceImpl();
+
+    /**
+     * Props to be initialized from application.props
+     **/
+    public static String DELIMITER;     // Delimiter for parsing inputs
+    public static String PARK;          // Prefix for park action
+    public static String UN_PARK;       // Prefix for un-park action
+    public static String COMPACT;       // Prefix for compact action
+
     public static void main(String[] args) {
+
+        // Initialize properties
+        try {
+            initializeProps();
+        } catch (FileNotFoundException ex) {
+            System.out.println("Properties file was not found.");
+            return;
+        } catch (IOException ex) {
+            System.out.println("Error while reading properties file.");
+            return;
+        }
+
         // Prompt user
         System.out.println("Please provide comma delimited list of actions... Type exit to end.");
 
@@ -20,7 +50,7 @@ public class Main {
             }
 
             // Split string to array
-            String[] inputArray = actionInputs.split(",");
+            String[] inputArray = actionInputs.split(DELIMITER);
 
             parseInputs(inputArray);
         }
@@ -41,19 +71,31 @@ public class Main {
             String firstChar = input.substring(0, 1).toLowerCase();
 
             // Handle accordingly
-            switch (firstChar) {
-                case "p":
-                    System.out.println("Parking");
-                    break;
-                case "u":
-                    System.out.println("un-Parking");
-                    break;
-                case "c":
-                    System.out.println("Compacting");
-                    break;
-                default:
-                    System.out.println("Unknown value");
+            if (firstChar.equals(PARK)) {
+                System.out.println("Parking");
+            } else if (firstChar.equals(UN_PARK)) {
+                System.out.println("un-Parking");
+            } else if (firstChar.equals(COMPACT)) {
+                System.out.println("Compacting");
+            } else {
+                System.out.println("An invalid prefix value was provided: " + firstChar.toLowerCase());
+                return;
             }
+
         }
+    }
+
+    /**
+     * Initialize variables with properties parsed from props file
+     *
+     * @throws IOException Exception caught while loading props file
+     */
+    private static void initializeProps() throws IOException {
+        propertyService.loadProps("application.properties");
+
+        DELIMITER = propertyService.getProp("delimiter");
+        PARK = propertyService.getProp("prefix.park");
+        UN_PARK = propertyService.getProp("prefix.un-park");
+        COMPACT = propertyService.getProp("prefix.compact");
     }
 }
