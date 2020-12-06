c7ee9fd13670bfefe7e80eb21995624e005b919c
Laptop-NIFU207\James <wjamesfl@gmail.com>
2020-12-06 16:13:37 -0500

    Add prompt, grab and split inputs

--- src/com/jpcodes/Main.java
+++ src/com/jpcodes/Main.java
@@ -1,8 +1,30 @@
 package com.jpcodes;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Prompt user
+        System.out.println("Please provide comma delimited list of actions... Type exit to end.");
+
+        // Set up a scanner for stdin and get input
+        Scanner scanner = new Scanner(System.in);
+        String actionInputs;
+
+        while (scanner.hasNext()) {
+            actionInputs = scanner.nextLine();
+
+            if (actionInputs.trim().equals("exit")) {
+                break;
+            }
+
+            // Split string to array
+            String[] inputArray = actionInputs.split(",");
+
+            // make sure we are getting the inputs.
+            System.out.println(Arrays.toString(inputArray));
+        }
     }
 }
